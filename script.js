38928dea65078728136958cfa40377ee6d01914f
Cian Hicks <cianhicks@gmail.com>
2021-12-08 10:14:45 -0500

    Adjust grid size

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
+
 const container = document.querySelector("#grid");
-console.log(container)
-   
-   let lengthGrid = 50;
-   for (let i = 0; i < lengthGrid; i++) {
+    // hardcoded grid size
+   let numberOfCells = 100;
+   // with cell size need to account for the border size of the cells
+   let cellSizeWidth = ((900 / numberOfCells) - (3));
+   let cellSizeHeight = ((900 / numberOfCells));
+
+
+   for (let i = 0; i < numberOfCells; i++) {
        const row = document.createElement('div');
        row.classList.add('row');
-       for (let t = 0; t < lengthGrid; t++) {
+       row.style.height = (cellSizeHeight) + "px";
+       row.style.width = (cellSizeWidth) + "px";
+       for (let t = 0; t < numberOfCells; t++) {
             const gridSquares = document.createElement('div');
             gridSquares.classList.add('gridSquare');
-            row.appendChild(gridSquares);           
+            gridSquares.style.height = cellSizeHeight + "px";
+            gridSquares.style.width = cellSizeWidth + "px";
+            row.appendChild(gridSquares);              
        }
        container.appendChild(row);
    }
-
+   
+   //for rainbow set this event listener to the rainbow function.
       container.addEventListener('mouseover', function(e) {
-        e.target.style.backgroundColor = "red"
-   })
+        e.target.style.backgroundColor = "blue"
+})
 
-    //dynamically set width of main-container
-   document.getElementById("main-container").style.width = (lengthGrid * 18 + 'px')
+    function gridSize(){
+        let gs = prompt("Enter the grid size (1-100)")
+        if(gs < 0 || gs > 100){
+            alert("ERROR: Enter a number between 1 - 100")
+        }else{
+            numberOfCells = gs
+            console.log("num cells: " + numberOfCells)
+            return numberOfCells
+        }
+        
+        let cellSize = 960 / gs;
+        console.log(cellSize)
+    }
+    
+    function rainbow(){
 
+    }
 
  // TO DO: 
  // Add option for user to select the grid size. (drop down menu or slider)
